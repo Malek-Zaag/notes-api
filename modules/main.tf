135da010d5e4a2231033090b4fe958f465f9dec7
Malek-Zaag <zaag.malek1@gmail.com>
2023-02-10 21:59:14 +0100

    working on tf

--- modules/main.tf
+++ modules/main.tf
@@ -45,5 +45,6 @@ resource "azurerm_app_service_source_control" "my-source-control" {
   app_id                 = azurerm_linux_web_app.webapp.id
   repo_url               = "https://github.com/Malek-Zaag/notes-api"
   branch                 = "main"
-  use_manual_integration = true
+  use_manual_integration = false
+
 }
