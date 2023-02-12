1f3ed9457019ef532a509abaaf66ac7c1bf3b9df
Malek-Zaag <zaag.malek1@gmail.com>
2023-02-12 22:20:33 +0100

    still working on tf module

--- modules/main.tf
+++ modules/main.tf
@@ -14,36 +14,131 @@ provider "azurerm" {
   subscription_id = var.subscription_id
 }
 
-resource "azurerm_resource_group" "my-first-azure-resource-group" {
-  name     = "example-webapps"
+resource "azurerm_resource_group" "notes-api-azure-resource-group" {
+  name     = "my-notes-api-rg"
   location = "West Europe"
 }
 
-# Create the Linux App Service Plan
-resource "azurerm_service_plan" "appservice-plan" {
-  name                = "webapp-asp-1"
-  resource_group_name = azurerm_resource_group.my-first-azure-resource-group.name
-  location            = azurerm_resource_group.my-first-azure-resource-group.location
-  os_type             = "Linux"
-  sku_name            = "B1"
+resource "azurerm_virtual_network" "notes-api-virtual-network" {
+  name                = "my-virtual-network"
+  location            = azurerm_resource_group.notes-api-azure-resource-group.location
+  address_space       = ["10.0.0.0/16"]
+  resource_group_name = azurerm_resource_group.notes-api-azure-resource-group.name
 }
 
-# Create the web app, pass in the App Service Plan ID
-resource "azurerm_linux_web_app" "webapp" {
-  name                = "webapp-1477"
-  resource_group_name = azurerm_resource_group.my-first-azure-resource-group.name
-  location            = azurerm_resource_group.my-first-azure-resource-group.location
-  service_plan_id     = azurerm_service_plan.appservice-plan.id
-  https_only          = false
-  site_config {
-    minimum_tls_version = "1.2"
+resource "azurerm_subnet" "notes-api-subnet" {
+  name                 = "my-subnet"
+  resource_group_name  = azurerm_resource_group.notes-api-azure-resource-group.name
+  virtual_network_name = azurerm_virtual_network.notes-api-virtual-network.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+#public ip for azure loadbalancer
+resource "azurerm_public_ip" "notes-api-public-ip" {
+  name                = "my-public-ip"
+  resource_group_name = azurerm_resource_group.notes-api-azure-resource-group.name
+  location            = azurerm_resource_group.notes-api-azure-resource-group.location
+  allocation_method   = "Static"
+}
+
+#the loadbalancer itself
+resource "azurerm_lb" "notes-api-loadbalancer" {
+  name                = "my-azure-loadbalancer"
+  resource_group_name = azurerm_resource_group.notes-api-azure-resource-group.name
+  location            = azurerm_resource_group.notes-api-azure-resource-group.location
+  frontend_ip_configuration {
+    name                 = "publicIPAddress"
+    public_ip_address_id = azurerm_public_ip.notes-api-public-ip.id
   }
 }
 
-#  Deploy code from a public GitHub repo
-resource "azurerm_app_service_source_control" "my-source-control" {
-  app_id                 = azurerm_linux_web_app.webapp.id
-  repo_url               = "https://github.com/Malek-Zaag/notes-api"
-  branch                 = "main"
-  use_manual_integration = false
+resource "azurerm_lb_backend_address_pool" "notes-api-lb-backend-address-pool" {
+  loadbalancer_id = azurerm_lb.notes-api-loadbalancer.id
+  name            = "BackEndAddressPool"
 }
+
+resource "azurerm_network_interface" "notes-api-network-interface" {
+  count               = 2
+  name                = "acctni${count.index}"
+  resource_group_name = azurerm_resource_group.notes-api-azure-resource-group.name
+  location            = azurerm_resource_group.notes-api-azure-resource-group.location
+  ip_configuration {
+    name                          = "testConfiguration"
+    subnet_id                     = azurerm_subnet.notes-api-subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_availability_set" "notes-api-aset" {
+  name                         = "my-availability-set"
+  location                     = azurerm_resource_group.notes-api-azure-resource-group.location
+  resource_group_name          = azurerm_resource_group.notes-api-azure-resource-group.name
+  platform_fault_domain_count  = 2
+  platform_update_domain_count = 2
+  managed                      = true
+}
+
+resource "azurerm_linux_virtual_machine" "notes-api-vm" {
+  count                 = 2
+  name                  = "acctvm${count.index}"
+  location              = azurerm_resource_group.notes-api-azure-resource-group.location
+  availability_set_id   = azurerm_availability_set.notes-api-aset.id
+  resource_group_name   = azurerm_resource_group.notes-api-azure-resource-group.name
+  network_interface_ids = [element(azurerm_network_interface.notes-api-network-interface.*.id, count.index)]
+  size                  = "Standard_B1s"
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file(var.public_key_file)
+  }
+
+  os_disk {
+    name                 = "os_disk_${count.index}"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  computer_name                   = "myvm${count.index}"
+  admin_username                  = "azureuser"
+  disable_password_authentication = true
+
+  provisioner "remote-exec" {
+    inline = [
+      "export PORT=${var.PORT}",
+      "export DB_URI=${var.DB_URI}",
+      "sudo apt update",
+      "curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash",
+      "chmod +x ~/.nvm/nvm.sh",
+      "source ~/.bashrc",
+      "nvm install 16",
+      "node -v",
+      "sudo apt install npm -y",
+      "sudo apt install git -y",
+      "mkdir /app",
+      "cd app ",
+      "git clone ${var.repo_url}",
+      "cd notes-api",
+      "npm i ",
+      "npm run start:dev",
+    ]
+    connection {
+      type        = "ssh"
+      user        = "azureuser"
+      port        = 22
+      host        = azurerm_public_ip.notes-api-public-ip.ip_address
+      private_key = file(var.private_key_file)
+    }
+
+  }
+
+  tags = {
+    environment = "production :D"
+  }
+}
+
--- modules/variables.tf
+++ modules/variables.tf
@@ -1,4 +1,6 @@
-# variable "global_se" {}
-# variable "resource_groups" {}
-variable "subscription_id" {
-}
+variable "subscription_id" {}
+variable "public_key_file" {}
+variable "repo_url" {}
+variable "DB_URI" {}
+variable "PORT" {}
+variable "private_key_file" {}
